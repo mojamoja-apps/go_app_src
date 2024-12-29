ea1e0e883100d70c4e1d4d24e8e12a6c0370f052
k-ohtsuka mojamoja apps <sorega.dorohedoro@gmail.com>
2024-12-29 17:22:26 +0900

    サンプルアプリ作成中

--- go_app/main.go
+++ go_app/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"go_app/model"
 )
 
 func main() {
 
 	r := gin.Default()
-	r.GET("/users", func(c *gin.Context) {
+
+	// CORS設定を追加
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	r.GET("/user", func(c *gin.Context) {
 		// データを取得
-		data, err := getData()
+		users, err := model.GetUsers()
 		if err != nil {
 			// エラーレスポンス
 			c.JSON(http.StatusInternalServerError, gin.H{
@@ -21,7 +34,30 @@ func main() {
 
 		// JSONでデータを返す
 		c.JSON(http.StatusOK, gin.H{
-			"data": data,
+			"data": users,
+		})
+	})
+
+	r.GET("/station", func(c *gin.Context) {
+		// クエリパラメータを取得
+		neLat, _ := strconv.ParseFloat(c.Query("ne_lat"), 64)
+		neLng, _ := strconv.ParseFloat(c.Query("ne_lng"), 64)
+		swLat, _ := strconv.ParseFloat(c.Query("sw_lat"), 64)
+		swLng, _ := strconv.ParseFloat(c.Query("sw_lng"), 64)
+
+		// データを取得
+		stations, err := model.GetStations(neLat, neLng, swLat, swLng)
+		if err != nil {
+			// エラーレスポンス
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		// JSONでデータを返す
+		c.JSON(http.StatusOK, gin.H{
+			"data": stations,
 		})
 	})
 	r.Run()
